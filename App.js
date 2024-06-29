ea5685aa5de7ced3a3b275adc3f54d084fedae4a
KevinCosma <kcosman2005@gmail.com>
2024-06-29 02:23:02 -0700

    Took out alignItems and justifyContent out of every style except for styles.container. Added another styles.headerText. Most importantly, I added a ScrollView. Will need to style that as well to make sure it works on both iPhone and Android

--- App.js
+++ App.js
@@ -5,55 +5,57 @@ import React from 'react';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.mainScreen}>
-        <Text style={styles.headerText}>Main Screen</Text>
-        <Text>Register</Text>
-        <Text>Login</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.mainScreen}>
+          <Text style={styles.headerText}>Main Screen</Text>
+          <Button title='Login'/>
+          <Button title='Register'/>
+        </View>
 
-      <View style={styles.loginScreen}>
-        <Text style={styles.headerText}>Login Screen</Text>
-        <TextInput placeholder='Username'/>
-        <TextInput placeholder='Password'/>
-      </View>
+        <View style={styles.loginScreen}>
+          <Text style={styles.headerText}>Login Screen</Text>
+          <TextInput placeholder='Username'/>
+          <TextInput placeholder='Password'/>
+        </View>
 
-      <View style={styles.registerScreen}>
-        <Text style={styles.headerText}>Register Screen</Text>
-        <TextInput placeholder='First Name'/>
-        <TextInput placeholder='Last Name'/>
-        <TextInput placeholder='Email'/>
-        <TextInput placeholder='Username'/>
-        <TextInput placeholder='Password'/>
-        <TextInput placeholder='Confirm Password'/>
-      </View>
+        <View style={styles.registerScreen}>
+          <Text style={styles.headerText}>Register Screen</Text>
+          <TextInput placeholder='First Name'/>
+          <TextInput placeholder='Last Name'/>
+          <TextInput placeholder='Email'/>
+          <TextInput placeholder='Username'/>
+          <TextInput placeholder='Password'/>
+          <TextInput placeholder='Confirm Password'/>
+        </View>
 
-      <View style={styles.userMainScreen}>
-        <Text style={styles.headerText}>UserMainScreen</Text>
-        <Button title='Search for a card'/>
-        <Button title='Add a card'/>
-        <Text>List Of Your Cards</Text>
-      </View>
+        <View style={styles.userMainScreen}>
+          <Text style={styles.headerText}>UserMainScreen</Text>
+          <Button title='Search for a card'/>
+          <Button title='Add a card'/>
+          <Text>List Of Your Cards</Text>
+        </View>
 
-      <View style={styles.addCardScreen}>
-        <Text style={styles.headerText}>Add Card Screen</Text>
-        <Text>Choose Card Game</Text>
-        <TextInput placeholder='Card Name'/>
-        <Text>Choose Damage Level</Text>
-        <TextInput placeholder='Price'/>
-        <Text>(Anything else we should add???)</Text>
-      </View>
+        <View style={styles.addCardScreen}>
+          <Text style={styles.headerText}>Add Card Screen</Text>
+          <Text>Choose Card Game</Text>
+          <TextInput placeholder='Card Name'/>
+          <Text>Choose Damage Level</Text>
+          <TextInput placeholder='Price'/>
+          <Text>(Anything else we should add???)</Text>
+        </View>
 
-      <View style={styles.searchScreen}>
-        <Text style={styles.headerText}>Search Screen</Text>
-        <Text>Filter TCG Dropdown</Text>
-        <Text>(Add filters that depend on which TCG you picked???)</Text>
-        <TextInput placeholder='Search'/>
-      </View>
+        <View style={styles.searchScreen}>
+          <Text style={styles.headerText}>Search Screen</Text>
+          <Text>Filter TCG Dropdown</Text>
+          <Text>(Add filters that depend on which TCG you picked???)</Text>
+          <TextInput placeholder='Search'/>
+        </View>
 
-      <View style={styles.discussion}>
-        <Text style={styles.headerText}>Message function?</Text>
-        <Text>Anything else that I haven't thought of???</Text>
-      </View>
+        <View style={styles.discussion}>
+          <Text style={styles.headerText}>Message function?</Text>
+          <Text style={styles.headerText}>Anything else that I haven't thought of???</Text>
+        </View>
+      </ScrollView>  
     </View>
   );
 }
@@ -68,51 +70,31 @@ const styles = StyleSheet.create({
 
   mainScreen: {
     padding: 20,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 
   loginScreen: {
     padding: 20,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 
   registerScreen: {
     padding: 20,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 
   userMainScreen: {
     padding: 20,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 
   addCardScreen: {
     padding: 20,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 
   searchScreen: {
     padding: 20,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 
   discussion: {
     padding: 20,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 
   headerText: {
