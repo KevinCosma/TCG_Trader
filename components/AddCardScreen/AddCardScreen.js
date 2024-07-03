2026238dc8f35663470dc135329cbcdb2ac65d4b
KevinCosma <kcosman2005@gmail.com>
2024-07-03 02:58:33 -0700

    Added a useState const, two functions, an onChangeText, and value to the AddCardScreen file

--- components/AddCardScreen/AddCardScreen.js
+++ components/AddCardScreen/AddCardScreen.js
@@ -2,13 +2,27 @@ import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import React from 'react';
 
 function addCardScreen() {
+    const [enteredCardName, setEnteredCardName] = useState('');
+
+    function cardNameInputHandler(enteredText) {
+        setEnteredCardName(enteredText)
+    }
+
+    function addCardButtonHandler() {
+
+    }
+
     return (
         <View style={styles.addCardScreen}>
             <Text style={styles.headerText}>Add Card Screen</Text>
             <Text>Choose Card Game</Text>
             <TextInput placeholder='Card Name'/>
             <Text>Choose Damage Level</Text>
-            <TextInput placeholder='Price'/>
+            <TextInput 
+                placeholder='Price'
+                onChangeText={cardNameInputHandler}
+                value={enteredCardName}
+            />
             <Text>(Anything else we should add???)</Text>
             <Button title='Add Card' />
         </View>
