fb8f0784cb0b56c2760712a7fa47e095049bf153
KevinCosma <kcosman2005@gmail.com>
2024-07-03 02:49:24 -0700

    Added a useState const, two functions, an onChangeText, and a value to SearchScreen file

--- components/SearchScreen/SearchScreen.js
+++ components/SearchScreen/SearchScreen.js
@@ -1,13 +1,27 @@
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 
 function searchScreen() {
+    const [enteredSearchInput, setEnteredSearchInput] = useState('');
+
+    function searchInputHandler(enteredText) {
+        setEnteredSearchInput(enteredText);
+    }
+
+    function searchButtonHandler() {
+        
+    }
+    
     return (
         <View style={styles.searchScreen}>
             <Text style={styles.headerText}>Search Screen</Text>
             <Text>Filter TCG Dropdown</Text>
             <Text>(Add filters that depend on which TCG you picked???)</Text>
-            <TextInput placeholder='Search'/>
+            <TextInput 
+                placeholder='Search'
+                onChangeText={searchInputHandler}
+                value={enteredSearchInput}    
+            />
             <Button title='Search'/>
       </View>
     );
